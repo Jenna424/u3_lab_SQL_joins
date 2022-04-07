2c0dd65bba3138a3d5c9ad3c05b5bce3fb7ceed3
Jenna Leopold <jleopold424@gmail.com>
2022-04-07 15:22:28 -0400

    updated solution to lab

--- real_estate_queries.sql
+++ real_estate_queries.sql
@@ -5,9 +5,9 @@ SELECT name, age, gender FROM tenants LIMIT 10;
 --Retrieve all info on all tenants older than 65
 SELECT * FROM tenants WHERE age > 65;
 --Retrieve all info on all tenants in apartment with id 20
-SELECT * FROM tenants JOIN apartments ON tenants.apartment_id = apartments.id WHERE apartments.id = 20;
+SELECT * FROM tenants WHERE apartment_id = 20;
 --Retrieve all info on all tenants in apartment with ids 20 or 21
-SELECT * FROM tenants JOIN apartments ON tenants.apartment_id = apartments.id WHERE apartments.id = 20 OR apartments.id = 21;
+SELECT * FROM tenants WHERE apartment_id = 20 OR apartment_id = 21;
 --Retrieve the names of all doormen and the address where they work
 SELECT doormen.name, buildings.address FROM doormen JOIN buildings ON doormen.building_id = buildings.id;
 --Geriatric Birthday! Update all tenants whose age is 90 to be 91
@@ -19,7 +19,6 @@ UPDATE doormen SET shift = 'Night' WHERE building_id = 3;
 --Create one new tenant, put them in any apartment you want
 INSERT INTO tenants (id, name, age, gender, apartment_id) VALUES (DEFAULT, 'Jenna Leopold', 30, 'Female', 1);
 --Find just the ids for all apartments for building with id of 2
-SELECT apartments.id FROM apartments JOIN buildings ON apartments.building_id = buildings.id WHERE buildings.id = 2;
+SELECT id FROM apartments WHERE building_id = 2;
 --Find all info for apartments in building number 3 whose price is greater than $2300
-SELECT * FROM apartments JOIN buildings ON apartments.building_id = buildings.id WHERE buildings.id = 3 AND apartments.price > 2300;
-
+SELECT * FROM apartments WHERE building_id = 3 AND price > 2300;
